1582505981deb7cb79bd0e8cb894b31fc92cbaa2
Md. Akash Ahmed <86203010+AkashAhmed41@users.noreply.github.com>
2022-07-15 04:32:52 +0600

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + "/views"));
 
 
 app.use(indexRoute);
